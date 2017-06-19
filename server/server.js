91c4aa4dd48b81dd5d5f9fe422ba22120a365cb6
rinkumalik11 <rinkumalik11@gmail.com>
2017-06-19 07:42:37 -0700

    New files are added

--- server/server.js
+++ server/server.js
@@ -1,61 +1,28 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var mongoose = require('./db/mongoose');
+var {Todo} = require('./models/Todo');
+var {User} = require('./models/User');
 
-var Todo = mongoose.model('Todo',{
-	text: {
-		type: String,
-		required: true,
-		minlength: 1,
-		trim: true
-	},
-	completed: {
-		type: Boolean,
-		default: false
-	},
-	completedAt: {
-		type: Number,
-		default: null
-	}
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req,res) => {
+	var todo = new Todo({
+		text: req.body.text
+	});
+
+	todo.save().then((r) => {
+		res.send(r);
+	},(e) => {
+		res.send(e);
+	})
 });
 
-var newTodo = new Todo({
-	text: 'New Todo'
+app.listen(3000, () => {
+	console.log('App is listening on port : 3000');
 });
 
-newTodo.save().then((res) => {
-	console.log('Data save');
-	console.log(res);
-},(err) => {
-	console.log('Unable to save todo',err);
-});
-
-//Create User model
-
-var User = mongoose.model('User',{
-	name: {
-		type: String,
-		required: true,
-		trim: true,
-		minlength: 1
-	},
-	email:{
-		type: String,
-		required: true,
-		trim: true,
-		minlength: 1
-	}
-});
-
-var newUser = new User({
-	name: 'NewUser',
-	email: 'newuser@email.com' 
-});
-
-newUser.save().then((userres) => {
-	console.log('User has been added successfully');
-	console.log(userres);
-},(err) => {
-	console.log('Unable to add user',err);
-});
